4004911813f41fd13de147a528e67f2bc60ee087
Nathan Wang <nathanbotinker@gmail.com>
2022-01-12 17:44:00 +1300

    test: replace jest fn

--- src/publish/index.test.ts
+++ src/publish/index.test.ts
@@ -1,6 +1,7 @@
 import { Temporal } from "https://cdn.skypack.dev/@js-temporal/polyfill?dts"
 import { publish, subscribe, subscribeToAllTopics, unsubscribe } from "../index.ts"
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import {PubSubEvent} from "../types.ts";
 
 	Deno.test("it returns an error when no event name", function () {
 		const topic = "blue"
@@ -19,105 +20,112 @@ import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 		assertEquals(err.message, "Published events must have a topic (event name).")
 	})
 
-	// Deno.test("it publishes correctly with topic", function () {
-	// 	const id = "my-id"
-	// 	const topic = "blue"
-	// 	const eventName = "PUBLISHED"
-	// 	const data = {
-	// 		color: "cyan",
-	// 	}
-	// 	const cb = jest.fn()
-	// 	const cbOnce = jest.fn()
-	//
-	// 	subscribe("jane", cb, { topic })
-	// 	subscribe("julie", cbOnce, { topic, once: true })
-	//
-	// 	publish(
-	// 		{
-	// 			eventName,
-	// 			data,
-	// 		},
-	// 		{
-	// 			topic,
-	// 		},
-	// 	)
-	//
-	// 	publish(
-	// 		{
-	// 			id,
-	// 			eventName,
-	// 			data,
-	// 		},
-	// 		{
-	// 			topic,
-	// 		},
-	// 	)
-	//
-	// 	publish(
-	// 		{
-	// 			id,
-	// 			eventName,
-	// 			data,
-	// 		},
-	// 		{
-	// 			topic: "green",
-	// 		},
-	// 	)
-	//
-	// 	const one = cb.mock.calls[0][0]
-	// 	const two = cb.mock.calls[1][0]
-	//
-	// 	assertEquals(one.id.length, 21)
-	// 	assertEquals(one.eventName, eventName)
-	// 	assertEquals(one.timestamp instanceof Temporal.ZonedDateTime, true)
-	// 	assertEquals(one.data, data)
-	//
-	// 	assertEquals(two.id, id)
-	// 	assertEquals(two.eventName, eventName)
-	// 	assertEquals(two.timestamp instanceof Temporal.ZonedDateTime, true)
-	// 	assertEquals(two.data, data)
-	//
-	// 	// expect(cbOnce).toHaveBeenCalledTimes(1)
-	// 	unsubscribe()
-	// })
-	//
-	// Deno.test("it publishes correctly without topic", function () {
-	// 	const id = "my-id"
-	// 	const eventName = "PUBLISHED"
-	// 	const data = {
-	// 		color: "cyan",
-	// 	}
-	// 	const cb = jest.fn()
-	// 	const cbOnce = jest.fn()
-	//
-	// 	subscribeToAllTopics("jane", cb, {})
-	// 	subscribeToAllTopics("julie", cbOnce, { once: true })
-	//
-	// 	publish(
-	// 		{
-	// 			eventName,
-	// 			data,
-	// 		},
-	// 		{},
-	// 	)
-	//
-	// 	publish({
-	// 		id,
-	// 		eventName,
-	// 		data,
-	// 	})
-	//
-	// 	const one = cb.mock.calls[0][0]
-	// 	const two = cb.mock.calls[1][0]
-	//
-	// 	assertEquals(one.id.length, 21)
-	// 	assertEquals(one.eventName, eventName)
-	// 	assertEquals(one.timestamp instanceof Temporal.ZonedDateTime, true)
-	// 	assertEquals(one.data, data)
-	//
-	// 	assertEquals(two.id, id)
-	// 	assertEquals(two.eventName, eventName)
-	// 	assertEquals(two.timestamp instanceof Temporal.ZonedDateTime, true)
-	// 	assertEquals(two.data, data)
-	// 	unsubscribe()
-	// })
+	Deno.test("it publishes correctly with topic", function () {
+		const id = "my-id"
+		const topic = "blue"
+		const eventName = "PUBLISHED"
+		const data = {
+			color: "cyan",
+		}
+
+		// TODO: Find a better way to create a mocked callback
+		let cbArgs = [] as PubSubEvent[]
+		const cb = (event: PubSubEvent) => {cbArgs.push(event)}
+		let cbOnceExecuteCount = 0
+		const cbOnce = (event: PubSubEvent) => {cbOnceExecuteCount++}
+
+		subscribe("jane", cb, { topic })
+		subscribe("julie", cbOnce, { topic, once: true })
+
+		publish(
+			{
+				eventName,
+				data,
+			},
+			{
+				topic,
+			},
+		)
+
+		publish(
+			{
+				id,
+				eventName,
+				data,
+			},
+			{
+				topic,
+			},
+		)
+
+		publish(
+			{
+				id,
+				eventName,
+				data,
+			},
+			{
+				topic: "green",
+			},
+		)
+
+		const one = cbArgs[0]
+		const two = cbArgs[1]
+
+		assertEquals(one?.id?.length, 21)
+		assertEquals(one.eventName, eventName)
+		assertEquals(one.timestamp instanceof Temporal.ZonedDateTime, true)
+		assertEquals(one.data, data)
+
+		assertEquals(two.id, id)
+		assertEquals(two.eventName, eventName)
+		assertEquals(two.timestamp instanceof Temporal.ZonedDateTime, true)
+		assertEquals(two.data, data)
+
+		assertEquals(cbOnceExecuteCount, 1)
+		unsubscribe()
+	})
+
+	Deno.test("it publishes correctly without topic", function () {
+		const id = "my-id"
+		const eventName = "PUBLISHED"
+		const data = {
+			color: "cyan",
+		}
+		// TODO: Find a better way to create a mocked callback
+		let cbArgs = [] as PubSubEvent[]
+		const cb = (event: PubSubEvent) => {cbArgs.push(event)}
+		let cbOnceExecuteCount = 0
+		const cbOnce = (event: PubSubEvent) => {cbOnceExecuteCount++}
+
+		subscribeToAllTopics("jane", cb, {})
+		subscribeToAllTopics("julie", cbOnce, { once: true })
+
+		publish(
+			{
+				eventName,
+				data,
+			},
+			{},
+		)
+
+		publish({
+			id,
+			eventName,
+			data,
+		})
+
+		const one = cbArgs[0]
+		const two = cbArgs[1]
+
+		assertEquals(one?.id?.length, 21)
+		assertEquals(one.eventName, eventName)
+		assertEquals(one.timestamp instanceof Temporal.ZonedDateTime, true)
+		assertEquals(one.data, data)
+
+		assertEquals(two.id, id)
+		assertEquals(two.eventName, eventName)
+		assertEquals(two.timestamp instanceof Temporal.ZonedDateTime, true)
+		assertEquals(two.data, data)
+		unsubscribe()
+	})
